a57f33eedd5f0d19c2510506261b69225963fb41
kingVora <vparth2003@gmail.com>
2025-09-30 20:29:42 +0530

    Add postgres dependency, create connection and update gitignore file

--- backend/sim/src/main/java/com/f1/sim/SimApplication.java
+++ backend/sim/src/main/java/com/f1/sim/SimApplication.java
@@ -1,12 +1,26 @@
 package com.f1.sim;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class SimApplication {
 
 	public static void main(String[] args) {
+		String projectRoot = Paths.get(System.getProperty("user.dir")).toString();
+		System.out.println(projectRoot);
+		Path envPath = Paths.get(projectRoot, ".env");
+		System.out.println("Exists: " + Files.exists(envPath));
+		Dotenv dotenv = Dotenv.configure().directory(projectRoot).ignoreIfMissing().load();
+		System.out.println(dotenv.get("DB_URL"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(SimApplication.class, args);
 	}
 
